55ba6f7acabbb52a4715f31732de0327c9ae68fe
GitWithChris <133349207+GitWithChris@users.noreply.github.com>
2023-06-05 15:25:21 +0200

    Crawler Update
    Infoseccrawler wurde um weitere Conditions erweiter, um mehr Challenges effektiv zu betrachten

--- Crawler_Infosecinstitute_Christoph.py
+++ Crawler_Infosecinstitute_Christoph.py
@@ -50,7 +50,7 @@ for page_num in range(num_pages):
         if 'walkthrough' not in challenge_url:                          # URL muss 'walkthrough' enthalten
             continue
         if 'ctf' not in challenge_url and 'capture-the-flag' not in challenge_url:      # zusätzlich muss die URL entweder 'ctf' oder 'capture-the-flag' enthalten
-            continue                                                    
+            continue
 
         # Send a request to the challenge page
         challenge_page = requests.get(challenge_url)
@@ -60,16 +60,24 @@ for page_num in range(num_pages):
         soup_challenge = BeautifulSoup(html_content_challenge, "html.parser")
 
         # Find all text nodes that contain '<<' and '>>'
-        relevant_tags = soup_challenge.find_all(string=re.compile(r'<<.*?>>'))      # alle text nodes in der HTML finden (.find_all(string)) mit einem vorgegebenen Muster (re.compile(r'<<.*?>>')). re.compile --> bei häufigen, ähnlichen Abfragen
+        relevant_tags = soup_challenge.find_all(string=re.compile(r'<<.*?>>'))  # Find relevant tags using existing method
 
         # Extract the commands from the tags
-        commands = [re.search(r'<<(.*?)>>', tag).group(1).strip() for tag in relevant_tags]     # nur die Inhalte extrahieren, welche zwischen '<< ' und ' >>' stehen. Diese anschließend an Liste anhängen
+        commands = [re.search(r'<<(.*?)>>', tag).group(1).strip() for tag in relevant_tags]
+
+        # Check if the commands list is empty
+        if not commands:
+            # Find all <em> tags in the HTML
+            em_tags = soup_challenge.find_all('em')
+
+            # Extract the relevant information from the <em> tags
+            commands = [em_tag.text.strip() for em_tag in em_tags]
 
         # Append the challenge information and commands to the list
-        extracted_information.append(f"Challenge Name: {challenge_name}")       # Challenge Name pro Iteration in Liste schreiben
-        extracted_information.append(f"Challenge URL: {challenge_url}")         # Challenge URL pro Iteration in Liste schreiben
+        extracted_information.append(f"Challenge Name: {challenge_name}")
+        extracted_information.append(f"Challenge URL: {challenge_url}")
         extracted_information.append("")
-        extracted_information.extend(commands)                                  # Extracted commands in Liste schreiben
+        extracted_information.extend(commands)
         extracted_information.append('\n\n')
 
         # Add the challenge URL and name to the set of unique challenges
@@ -85,7 +93,7 @@ for page_num in range(num_pages):
 script_directory = os.path.dirname(os.path.abspath(__file__))
 
 # Set the output file path
-output_file = os.path.join(script_directory, "extracted_commands_infosecinstitute_V2.txt")
+output_file = os.path.join(script_directory, "extracted_commands_infosecinstitute.txt")
 
 # Insert the challenge counter at the beginning of the extracted information
 extracted_information.insert(0, f"Total Challenges: {challenge_counter}\n\n")
