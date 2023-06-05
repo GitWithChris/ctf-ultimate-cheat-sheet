b33b4a47bf0fb53857c721c317d8a28e6ca7e4f2
GitWithChris <133349207+GitWithChris@users.noreply.github.com>
2023-06-05 11:07:54 +0200

    Cleaning Script Update

--- Cleaning_script.py
+++ Cleaning_script.py
@@ -1,89 +1,72 @@
-import difflib
 import os
+import re
 
-# Replace 'input_file.txt' with the actual file name
-input_file = './extracted_commands_infosecinstitute.txt'
+# Function to compare the first 5 letters of two strings
+def compare_strings(string1, string2):
+    return string1[:5] == string2[:5]
 
-# Create a new file name with '_cleaned' appended
-output_file = input_file.replace('.txt', '_cleaned.txt')
+# Name of the input files (wildcard for user input)
+input_file_name = 'extracted_commands_infosecinstitute.txt'
+linux_commands_file_name = 'linux_commands.txt'
 
-# Get a list of commands from the /bin directory
-bin_commands = os.listdir('/bin')
+# Percentage wildcard for matching strings
+match_percentage = 80  # You can change this value
 
-# Define the similarity threshold for fuzzy matching
-similarity_threshold = 0.5
+# Get the current directory
+current_dir = os.getcwd()
 
-# Define keywords for exceptions
-exceptions = ['Challenge Name', 'Challenge URL']
+# Construct the input file path
+input_file_path = os.path.join(current_dir, input_file_name)
 
-# Flag to indicate if the lines should be deleted
-delete_line = False
+# Construct the Linux commands file path
+commands_file_path = os.path.join(current_dir, linux_commands_file_name)
 
-# Counter for challenges
-total_challenges_pre_cleaning = 0
-total_challenges_after_cleaning = 0
+# Read the input file
+with open(input_file_path, 'r') as input_file:
+    input_lines = input_file.readlines()
 
-# List to store extracted lines
-extracted_lines = []
+# Read the Linux commands file
+with open(commands_file_path, 'r') as commands_file:
+    commands_lines = commands_file.readlines()
 
-# Function to check similarity using fuzzy matching
-def check_similarity(line, command):
-    return difflib.SequenceMatcher(None, line[:5], command[:5]).ratio()
+# Initialize the cleaned lines list
+cleaned_lines = []
 
-# Check if the input file exists
-if not os.path.isfile(input_file):
-    print(f"Input file '{input_file}' not found.")
-    exit()
+# Loop through each line of the input file
+for line in input_lines:
+    # Strip the line of leading/trailing whitespaces
+    line = line.strip()
+    
+    # Skip lines starting with 'Challenge Name' and 'Challenge URL'
+    if line.startswith('Challenge Name') or line.startswith('Challenge URL') or line == '':
+        cleaned_lines.append(line)
+        continue
+    
+    # Flag to check if any match is found
+    match_found = False
+    
+    # Loop through each line of the commands file
+    for command in commands_lines:
+        # Strip the command of leading/trailing whitespaces
+        command = command.strip()
+        
+        # Compare the first 5 letters of the current line and command
+        if compare_strings(line, command):
+            match_found = True
+            break
+    
+    # If a match is found, add the line to the cleaned lines
+    if match_found:
+        cleaned_lines.append(line)
 
-try:
-    # Open the input and output files
-    with open(input_file, 'r') as file_in, open(output_file, 'w') as file_out:
-        # Loop through each line in the input file
-        for line in file_in:
-            # Check if the line starts with an exception keyword
-            if line.startswith('Challenge Name'):
-                file_out.write(line.replace('Total Challenges', 'Total Challenges pre cleaning'))
-                try:
-                    total_challenges_pre_cleaning = int(line.split()[-1])
-                except ValueError:
-                    total_challenges_pre_cleaning = 0
-                continue
-            elif line.startswith('Challenge URL'):
-                file_out.write(line)
-                continue
-            
-            # Check if the line contains a command based on similarity with the available commands
-            if any(check_similarity(line, command) >= similarity_threshold for command in bin_commands):
-                # Write the line to the output file
-                file_out.write(line)
-                extracted_lines.append(line)
-                total_challenges_after_cleaning += 1
-            else:
-                # Set the flag to delete the line
-                delete_line = True
-            
-            # Check if the line contains the end of a command sequence
-            if line.strip() == 'cat r00t.txt':
-                # Reset the flag after the end of a command sequence
-                delete_line = False
-            
-            # Delete the line if the flag is set
-            if delete_line:
-                continue
-            
-            # Print the line to the console
-            print(line.strip())
+# Generate the output cleaned file name
+cleaned_file_name = input_file_name.rsplit('.', 1)[0] + '_cleaned.txt'
 
-    # Open the cleaned file and append extracted lines
-    with open(output_file, 'a') as file_out:
-        file_out.write('\n\nTotal Challenges after cleaning: ' + str(total_challenges_after_cleaning) + '\n\n')
-        file_out.write('Extracted Lines:\n')
-        for extracted_line in extracted_lines:
-            file_out.write(extracted_line)
-        file_out.write('\n\n')
+# Construct the cleaned file path
+cleaned_file_path = os.path.join(current_dir, cleaned_file_name)
 
-    print("Script execution completed successfully!")
+# Write the cleaned lines to the output file
+with open(cleaned_file_path, 'w') as cleaned_file:
+    cleaned_file.write('\n'.join(cleaned_lines))
 
-except IOError:
-    print(f"Error occurred while reading or writing the files.")
-    exit()
+print(f'Cleaned file "{cleaned_file_name}" has been generated.')
